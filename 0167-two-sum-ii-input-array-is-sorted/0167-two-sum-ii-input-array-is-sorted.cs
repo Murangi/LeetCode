56a8aebff98ddb41f80cf5ccdcb917135077a623
Murangi <65628645+Murangi@users.noreply.github.com>
2025-01-17 21:39:07 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        
+        int[] Ans = new int[2];
+        int NumbersLength = numbers.Length;
+
+        for(int i = 0 ; i < NumbersLength -1; i++)
+        {
+            for(int j = i+1; j < NumbersLength; j++)
+            {
+                if(numbers[i] + numbers[j] == target)
+                {
+                    Ans[0] = i+1;
+                    Ans[1] = j+1;
+                }
+            }
+        }
+
+        return Ans;
+        
+    }
+}
