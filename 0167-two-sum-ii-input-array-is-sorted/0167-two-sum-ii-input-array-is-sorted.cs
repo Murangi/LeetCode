5a4dbcc67686b3175b2d8754f66319fb7aec0bfd
Murangi <65628645+Murangi@users.noreply.github.com>
2025-01-18 11:08:35 +0200

    Time: 0 ms (100%), Space: 50.7 MB (8.04%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cs
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cs
@@ -1,26 +1,52 @@
+// public class Solution {
+//     public int[] TwoSum(int[] numbers, int target) {
+        
+//         //int[] Ans = new int[2];
+//         int NumbersLength = numbers.Length;
+
+//         for(int i = 0 ; i < NumbersLength -1; i++)
+//         {
+//             for(int j = i+1; j < NumbersLength; j++)
+//             {
+//                 if(numbers[i] + numbers[j] == target)
+//                 {
+//                     // Ans[0] = i+1;
+//                     // Ans[1] = j+1;
+
+//                     return new int[] {i+1, j+1};
+//                 }
+//             }
+//         }
+
+//         return new int[] {0, 0};
+
+//         //return Ans;
+        
+//     }
+// }
+
 public class Solution {
     public int[] TwoSum(int[] numbers, int target) {
         
-        //int[] Ans = new int[2];
-        int NumbersLength = numbers.Length;
+    int LeftPointer = 0;
+    int RightPointer = numbers.Length -1;
 
-        for(int i = 0 ; i < NumbersLength -1; i++)
+    while(LeftPointer < RightPointer)
+    {
+        if(numbers[LeftPointer] + numbers[RightPointer] > target)
         {
-            for(int j = i+1; j < NumbersLength; j++)
-            {
-                if(numbers[i] + numbers[j] == target)
-                {
-                    // Ans[0] = i+1;
-                    // Ans[1] = j+1;
-
-                    return new int[] {i+1, j+1};
-                }
-            }
+            RightPointer--;
         }
+        else if(numbers[LeftPointer] + numbers[RightPointer] < target)
+        {
+            LeftPointer++;
+        }
+        else
+        {
+            return new int[] {LeftPointer+1, RightPointer+1};
+        }
+    }  
 
-        return new int[] {0, 0};
-
-        //return Ans;
-        
-    }
+     return new int[] {0,0}; 
+    } 
 }
